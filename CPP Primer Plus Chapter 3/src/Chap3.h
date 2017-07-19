c0924a42b235eb865b279046c8d7b96d8af125c4
All Might <none@gmail.com>
2017-07-19 16:22:25 -0700

    initial header file

---
+++ CPP Primer Plus Chapter 3/src/Chap3.h
@@ -0,0 +1,21 @@
+/*
+ * Chap3.h
+ *
+ *  Created on: Jul 19, 2017
+ *      Author: Sh0gun
+ */
+
+#ifndef SRC_CHAP3_H_
+#define SRC_CHAP3_H_
+
+void exercise1();
+void exercise2();
+void exercise3();
+void exercise4();
+void exercise5();
+void exercise6();
+void exercise7();
+
+
+
+#endif /* SRC_CHAP3_H_ */
