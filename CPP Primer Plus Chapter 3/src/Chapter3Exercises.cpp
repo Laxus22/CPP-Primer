48c9561e7122f0d5e7383c4f73451e5a642288dd
All Might <none@gmail.com>
2017-07-19 13:32:58 -0700

    up ex 2

--- CPP Primer Plus Chapter 3/src/Chapter3Exercises.cpp
+++ CPP Primer Plus Chapter 3/src/Chapter3Exercises.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 
 void exercise1();
+void exercise2();
 
 int main() {
-	exercise1();
+//	exercise1();
+	exercise2();
 }
 
 //converts heigt inches to feet inches
@@ -11,7 +13,33 @@ void exercise1() {
 	using namespace std;
 	int heightInches;
 	const int inchPerFeet = 12;
-	std::cout << "Enter your height in inches: _" << std::endl;
+	std::cout << "Enter your height in inches: _";
 	std::cin >> heightInches;
-	cout<<"Height "<<heightInches/inchPerFeet<<" Feet "<<heightInches%inchPerFeet<<endl;
+	cout << "Height: " << heightInches / inchPerFeet << " Feet "
+			<< heightInches % inchPerFeet << " inches." << endl;
+}
+
+//calculate BMI
+void exercise2() {
+	using namespace std;
+	int heightInFeet, heightInInches, weightPound;
+	const int inchPerFoot = 12;
+	const float inchToMeterFactor = 0.0254;
+	const float poundToKiloFactor = 2.2;
+	//gather info.
+	cout << "Enter your height in feet. " << endl;
+	cin >> heightInFeet;
+	cout << "Enter your height in inches. " << endl;
+	cin >> heightInInches;
+	cout << "Enter your weight in pounds." << endl;
+	cin >> weightPound;
+
+	//conversion.
+	int totalHeightInInches = heightInFeet * inchPerFoot + heightInInches;
+	float heightInMeters=totalHeightInInches*inchToMeterFactor;
+	float mass = weightPound/poundToKiloFactor;
+
+	float BMI = mass/(heightInMeters*heightInMeters);
+
+	cout<<BMI;
 }
